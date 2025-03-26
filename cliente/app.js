c0d77ca96d337588ad83834ed5c467703f3a8ed8
johan pinto <johanpinto232@gmail.com>
2025-03-26 10:30:08 -0500

    practica api

--- cliente/app.js
+++ cliente/app.js
@@ -1,27 +1,49 @@
-// fetch("http://localhost:3000/posts").then((datos)=>{
-//   const posts=datos.json().then((posts)=>{
+// fetch("http://localhost:3000/posts").then((datos) => {
+//   const posts = datos.json().then((posts) => {
 //     console.log(posts);
-    
 //   });
-  
 // });
 
-  
-
-const guardar=()=>{
-  fetch(`https://jsonplaceholder.typicode.com/posts`,{
-    method: 'POST',
+const guardar = () => {
+  fetch(`http://localhost:3000/posts`, {
+    method: "POST",
     body: JSON.stringify({
-      title: 'ADSO2894667',
-      body: 'johan',
-      
+      title: "ADSO2894667",
+      body: "johan",
     }),
+
     headers: {
-      'Content-type': 'application/json; charset=UTF-8',
+      "Content-type": "application/json; charset=UTF-8",
     },
   })
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-}
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+  // fetch("http://localhost:3000/comments", {
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //     body: "juan",
+  //     postId: 1,
+  //   }),
+  //   headers: {
+  //     "Content-type": "application/json; charset=UTF-8",
+  //   },
+  // })
+  //   .then((response) => response.json())
+  //   .then((json) => console.log(json));
+
+  // fetch("http://localhost:3000/profile", {
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //     body: "carlos",
+  //     postId: 1,
+  //   }),
+  //   headers: {
+  //     "Content-type": "application/json; charset=UTF-8",
+  //   },
+  // })
+  //   .then((response) => response.json())
+  //   .then((json) => console.log(json));
+};
 
 guardar();
